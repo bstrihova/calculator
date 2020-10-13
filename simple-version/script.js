b804e46786b98cca96a6ad3c4cef2ec6ce28492c
bstrihova <bara@MacBook-Pro.local>
2020-10-13 12:13:05 +0200

    deleted unnecessary files

--- simple-version/script.js
+++
@@ -1,8 +0,0 @@
-let result = 0;
-
-function addNumber() {
-  // using const coz u don't change the add value in this function
-  const add = Number(document.getElementById("add").value);
-  result += add;
-  document.getElementById("result").textContent = result;
-}
